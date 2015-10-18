40912a5c03481cb3205e1bff64347ffb4082f404
IronFox <github@portal.delta-works.org>
2015-10-18 16:01:33 +0200

    Added session to keep track of session-global variables

---
+++ Session.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechniteLogic
+{
+	public static class Session
+	{
+		public static UInt32 roundNumber = 0;
+	}
+}
